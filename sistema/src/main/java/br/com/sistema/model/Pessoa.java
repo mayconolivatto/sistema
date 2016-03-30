18c488aabf3a6d9099a9b75c36c13f75de3d1b3d
Milks <Milks@Grotle>
2016-03-30 14:49:05 -0300

    Criação do atributo código com get e set.

--- sistema/src/main/java/br/com/sistema/model/Pessoa.java
+++ sistema/src/main/java/br/com/sistema/model/Pessoa.java
@@ -2,4 +2,14 @@ package br.com.sistema.model;
 
 public abstract class Pessoa {
 
+	private long codigo;
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	
 }
