a18dd79854e85ae86552580bc470445475db1b0a
Milks <Milks@Grotle>
2016-03-31 13:40:16 -0300

    Atributos, getters e setters adicionados na classe Usuario.

--- sistema/src/main/java/br/com/sistema/model/Usuario.java
+++ sistema/src/main/java/br/com/sistema/model/Usuario.java
@@ -1,5 +1,50 @@
 package br.com.sistema.model;
 
-public class Usuario {
+public class Usuario extends Funcionario{
 
+		/***
+		 * Atributos de usuario, serão usados para o usuario conseguir fazer login no sistema
+		 * 
+		 */
+		private String usuario;
+		private String senha;
+		private String tipo;
+		
+		/***
+		 * Construtor vazio, decidir posteriormente.
+		 */
+		public Usuario(){
+			super();
+		}
+
+		/***
+		 * Getters e setters de Usuario
+		 * @return
+		 */
+		public String getUsuario() {
+			return usuario;
+		}
+
+		public void setUsuario(String usuario) {
+			this.usuario = usuario;
+		}
+
+		public String getSenha() {
+			return senha;
+		}
+
+		public void setSenha(String senha) {
+			this.senha = senha;
+		}
+
+		public String getTipo() {
+			return tipo;
+		}
+
+		public void setTipo(String tipo) {
+			this.tipo = tipo;
+		}
+		
+		
+		
 }
